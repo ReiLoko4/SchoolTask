eb4e6445824624907ae1e54af62e0743ad27ae71
"Thiago Viana Vargas" <thiagovviphone@gmail.com>
2023-02-23 08:22:38 -0300

    Adicionado funcionalidade digitar o nome e imprimir na tela

--- TRABALHO_GIT_2K/src/Main.java
+++ TRABALHO_GIT_2K/src/Main.java
@@ -1,9 +1,13 @@
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner t = new Scanner(System.in);
+		System.out.println("Digite seu nome: ");
+		String a = t.nextLine();
+		System.out.println("Olá " + a);
 		System.out.println("Olá Mundo");
-
+		
 	}
 
 }
